3b32d7e5b064273054d47ee6a6a861656273cef9
Emma's <emmazhang@Emma.attlocal.net>
2023-08-04 16:41:06 -0700

    update registration

---
+++ authentication/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('registration', views.registration)
+]
+
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def registration(request):
+    logger.info("Registration view accessed.")
+    if request.method == "POST":
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            logger.info("Form is valid. Saving user.")
+            user_form.save()
+            logger.info("User saved successfully.")
+            return HttpResponse("<h1>Registration successfully</h1>")
+        else:
+            logger.info("Form is invalid: %s", user_form.errors)
+    else:
+        user_form = UserCreationForm()
+    return render(request, 'registration.html', {'user_form' : user_form})
