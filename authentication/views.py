e94398997fc025b0cca1e48cd9c20f6d5c16fcb7
kimElloe <kiiru@elloe.ai>
2023-08-15 21:33:31 +0300

    added login

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
-from django.contrib.auth.forms import UserCreationForm
 import logging
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import HttpResponse, render
 
 logger = logging.getLogger(__name__)
 
@@ -22,3 +24,24 @@ def registration(request):
     else:
         user_form = UserCreationForm()
     return render(request, 'registration.html', {'user_form' : user_form})
+
+
+def login(request):
+    logger.info("Login view accessed.")
+    if request.method == "POST":
+        login_form = AuthenticationForm(request, data=request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                logger.info("User logged in: %s", username)
+                return HttpResponse("<h1>Login successful</h1>")
+            else:
+                logger.info("Invalid login credentials for user: %s", username)
+        else:
+            logger.info("Login form is invalid: %s", login_form.errors)
+    else:
+        login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
