558fdef647e76ebdefc7339cee9e3543343b7946
kimElloe <kiiru@elloe.ai>
2023-08-21 19:02:25 +0300

    revamp bliss

--- authentication/views.py
+++ authentication/views.py
@@ -1,47 +1,30 @@
-import logging
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.shortcuts import HttpResponse, render
 
-logger = logging.getLogger(__name__)
+def home(request):
+    return render(request, "home.html")
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
 
-def registration(request):
-    logger.info("Registration view accessed.")
+def register(request):
     if request.method == "POST":
-        user_form = UserCreationForm(request.POST)
-        if user_form.is_valid():
-            logger.info("Form is valid. Saving user.")
-            user_form.save()
-            logger.info("User saved successfully.")
-            return HttpResponse("<h1>Registration successfully</h1>")
-        else:
-            logger.info("Form is invalid: %s", user_form.errors)
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Hi {username}, your account was created successfully"
+            )
+            return redirect("home")
     else:
-        user_form = UserCreationForm()
-    return render(request, 'registration.html', {'user_form' : user_form})
+        form = UserRegisterForm()
+
+    return render(request, "register.html", {"form": form})
 
 
-def login(request):
-    logger.info("Login view accessed.")
-    if request.method == "POST":
-        login_form = AuthenticationForm(request, data=request.POST)
-        if login_form.is_valid():
-            username = login_form.cleaned_data.get('username')
-            password = login_form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                logger.info("User logged in: %s", username)
-                return HttpResponse("<h1>Login successful</h1>")
-            else:
-                logger.info("Invalid login credentials for user: %s", username)
-        else:
-            logger.info("Login form is invalid: %s", login_form.errors)
-    else:
-        login_form = AuthenticationForm()
-    return render(request, 'login.html', {'login_form': login_form})
+@login_required()
+def profile(request):
+    return render(request, "profile.html")
--- bliss/settings.py
+++ bliss/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -117,11 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
